9843638a4cbe71b82cb4e19519bb84fbeb63bd79
Mystic <187439614+StriverV@users.noreply.github.com>
2025-05-04 00:08:03 +0600

    Create Div2_Translation.cpp

---
+++ Div2_Translation.cpp
@@ -0,0 +1,23 @@
+
+#include<bits/stdc++.h>
+#define Naba ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long 
+#define bl bool 
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+using namespace std;
+
+int main()
+{
+    Naba;
+
+    string s,t;
+    cin >> s >> t;
+    
+    reverse(s.begin(), s.end());
+    
+    s == t? yes: no;
+
+    return 0;
+}

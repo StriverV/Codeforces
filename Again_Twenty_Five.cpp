3bbeb44dd651f2df58fd6fa1596157e36c220fdf
Mystic <187439614+StriverV@users.noreply.github.com>
2025-05-13 19:22:36 +0600

    Create Again_Twenty_Five.cpp

---
+++ Again_Twenty_Five.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define Naba ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long 
+#define nl '\n'
+
+using namespace std;
+
+int main()
+{
+    Naba;
+
+    int n;
+    cin >> n;
+    
+    cout << "25" << nl;
+    
+    return 0;
+}
+
+

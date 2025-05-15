f33cd1249745b5ce965f3b58e5656df666d86f85
Mystic <187439614+StriverV@users.noreply.github.com>
2025-05-15 20:25:02 +0600

    Create adding_bits.cpp

---
+++ adding_bits.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define Naba ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0); 
+#define nl '\n'
+using namespace std;
+
+int main()
+{
+    Naba;
+
+    int a,b;
+    cin >> a >> b;
+
+    cout << (a^b) << nl;
+
+    return 0;
+}

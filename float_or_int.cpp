c05329a20ff3ecfd8a38d1b15d0abc1c4b54c42a
Mystic <187439614+StriverV@users.noreply.github.com>
2025-05-15 19:08:49 +0600

    Create float_or_int.cpp

---
+++ float_or_int.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define Naba ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long 
+#define bl bool 
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+using namespace std;
+
+int main()
+{
+    Naba;
+
+    double a;
+    cin >> a;
+
+
+    a == int(a) ? cout << "int " << int(a) << nl : cout << "float " << int(a) <<" "<< a - int(a) << nl;
+    return 0;
+}

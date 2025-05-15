98c9e29b5154cd29877af2b76605630b10efdf09
Mystic <187439614+StriverV@users.noreply.github.com>
2025-05-15 19:20:29 +0600

    Create The_Last_two_digits.cpp

---
+++ The_Last_two_digits.cpp
@@ -0,0 +1,33 @@
+
+#include<bits/stdc++.h>
+#define Naba ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long 
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define mod 1000000007
+using namespace std;
+
+int main()
+{
+    Naba;
+
+    vector<ll> v;
+    ll p = 1;
+    for (int i = 0; i < 4; i++)
+    {
+        int x;  cin >> x;
+        v.push_back(x);
+
+        p *= v[i];
+        p %= mod;
+    }
+ 
+    string s = to_string(p) ;
+
+    cout << s[s.size() - 2] <<  s[s.size() - 1] << nl;
+
+    return 0;
+}
+
+

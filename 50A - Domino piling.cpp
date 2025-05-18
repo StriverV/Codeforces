24979038363a8d0639831bff282631b1a0888d78
Mystic <187439614+StriverV@users.noreply.github.com>
2025-05-18 20:32:54 +0600

    Create 	50A - Domino piling.cpp

---
+++ 50A - Domino piling.cpp
@@ -0,0 +1,26 @@
+
+#include<bits/stdc++.h>
+#define Naba ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define ll long long 
+#define bl bool 
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+using namespace std;
+
+int main()
+{
+    Naba;
+
+    int m,n;
+    cin >> m >> n;
+
+    int a = m*n;  
+
+    int s = 2;
+  
+    cout << a/s << nl;
+    
+
+    return 0;
+}
